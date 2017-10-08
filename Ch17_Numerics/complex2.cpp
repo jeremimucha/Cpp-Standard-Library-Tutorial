45137f2fe6bd360f2887ed5b9d5ea73310089a54
rockpaperbanan <rockpaperbanan@gmail.com>
2017-10-08 12:49:35 +0200

    Ch17 exapmles

---
+++ Ch17_Numerics/complex2.cpp
@@ -0,0 +1,38 @@
+// Chapter 17 Numerics
+#include <iostream>
+#include <complex>
+#include <cstdlib>
+#include <limits>
+using namespace std;
+
+
+int main()
+{
+    complex<long double> c1, c2;
+
+    while(cin.peek() != EOF){
+        // read first complex number
+        cout << "complex number c1: ";
+        cin >> c1;
+        if( !cin ){
+            cerr << "input error" << endl;
+            return EXIT_FAILURE;
+        }
+        // read second complex number
+        cout << "complex number c2: ";
+        cin >> c2;
+        if( !cin ){
+            cerr << "input error" << endl;
+            return EXIT_FAILURE;
+        }
+
+        if( c1 == c2 ){
+            cout << "c1 and c2 are equal !" << endl;
+        }
+        cout << "c1 raised to the c2: " << pow(c1, c2)
+             << endl << endl;
+        
+        // skip the rest of line
+        cin.ignore( numeric_limits<int>::max(), '\n' );
+    }
+}
